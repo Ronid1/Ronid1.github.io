9e986e0aa84bc913280651b615a63559d5eafcd6
Ronid1 <ronidiwan@gmail.com>
2023-11-14 10:27:08 -0800

    update data

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,25 @@
 import Social from "./Social";
-import memoji from "../static/memoji.png"
+import memoji from "../static/memoji.png";
+import { aboutMe } from "../data/aboutData";
 
 function About(props) {
-  const about = "I'm a Tel Aviv based Full-Stack Software Engineer. I’m passionate about good design; I believe code should be clean, scalable and easy to read and apps should be intuitive and easy to use. I view software development as the ultimate mesh between my creative and technical skills and I am eager to learn and grow in this field."
-
     return (
-      <div class="section">
-        <h1>Who I am</h1>
-        <div class="container container-tight text-center">
-          <div class="row align-items-center">
-            <div class="col-md-8">
-              <br />
-              <p>{about}</p>
-              <Social />
+        <div class="section">
+            <h1>Who I am</h1>
+            <div class="container container-tight text-center">
+                <div class="row align-items-center">
+                    <div class="col-md-8">
+                        <br />
+                        <p>{aboutMe}</p>
+                        <Social />
+                    </div>
+                    <div class="col-md-4">
+                        <img src={memoji} class="img-sm" alt="my_emoji" />
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4">
-              <img src={memoji} class="img-sm"/>
-            </div>
-          </div>
         </div>
-      </div>
     );
-  }
-  
-  export default About;
+}
+
+export default About;
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,5 +1,5 @@
 export const skills = [
-  { name: "Creativity", description: "I view problems as opportunities and enjoy challenging myself in finding elegant and practical solutions" },
-  { name: "Quality", description: "I believe code should be clean, scalable and easy to read and apps should be intuitive and easy to use" },
-  { name: "Toolkit", description: "I bring knowledge in software engineering, technologies and multiple coding languages, acquired both at university and self-taught" },
+    { name: "Creativity", description: "I view problems as opportunities and enjoy challenging myself in finding elegant and practical solutions" },
+    { name: "Quality", description: "I am an advocate for clean, scalable, testable, thoughtful and well designed code" },
+    { name: "Toolkit", description: "I have knowledge in software engineering, technologies and multiple coding languages, acquired both at university and self-taught" },
 ];
