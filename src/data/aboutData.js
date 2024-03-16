e8c7c0f6e24e98c3603153b7410bcc2a83ff7bc1
Ronid1 <ronidiwan@gmail.com>
2024-03-15 17:48:31 -0700

    fix typo

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,3 +1,3 @@
 export const aboutMe = `I'm a Greater Vancouver based Full-Stack Software Engineer, 
-    strong self-learner and high achiever with a background in design and project management and a passion for software design and user experience.
+    strong self-learner and high achiever with a background in design and project management, and a passion for software design and user experience.
     I view software development as the ultimate mesh between my creative and technical skills.`;
