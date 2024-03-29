368e5e1c6d00d92126d48ef81b0f9f6baec5bdf5
Ronid1 <ronidiwan@gmail.com>
2024-03-29 14:17:11 -0700

    add open source contributions

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,43 +1,50 @@
 export const projects = [
-    {
-        name: "Certification Portal",
-        tech: "React.JS | Django",
-        description:
-            "Implemented a complete Software Development Life Cycle including analysis, architecture, design and execution. Focusing on design patterns and object-oriented principals.",
-        link: "https://github.com/Ronid1/Certification_Portal",
-        demoGif: "Certs_Portal",
-        imageName: "Certs_Portal",
-    },
-    {
-        name: "Rubiks Cube",
-        tech: "JavaScript | Three.JS",
-        description:
-            "Rubiks cube simulator developed with 3D modeling and user interaction.",
-        link: "https://github.com/Ronid1/Rubiks_Cube",
-        demoGif: "Rubiks_cube",
-        imageName: "Rubiks_cube",
-    },
-    {
-        name: "Wordle",
-        tech: "React.JS | Next.JS",
-        description:
-            "Extended Wordle game with a flexible board size. Designed components using JSX and CSS and managed game logic with React Context. Built a Next.JS server to fetch words and cross-check guesses.",
-        link: "https://github.com/Ronid1/Wordle",
-        demoGif: "wordle",
-        imageName: "Wordle",
-    },
-    {
-        name: "Maps backend",
-        tech: "Node.JS | Express.JS | MongoDB",
-        description:
-            "Full server-side application including managing the database in MongoDB and building a RESTful API with end-point routing, data validation, error handling and integration of an external API.",
-        link: "https://github.com/Ronid1/Maps_Backend",
-    },
-    {
-        name: "This Web Page",
-        tech: "React.JS | CSS",
-        description:
-            "Frontend focused project, built in react and designed using some Bootstrap and lots of CSS. Desktop and mobile friendly.",
-        link: "https://github.com/Ronid1/Ronid1.github.io",
-    },
+  {
+    name: "Open-Source Contributions",
+    tech: "JavaScript | TypeScript",
+    description:
+      "My contributions to Mermaid.JS as a top 10% developer, including new features I originated and implemented, refactors and bug fixes, updated documentation and unit & e2e integration tests with Jest and Cypress.",
+    link: "https://github.com/mermaid-js/mermaid/pulls?q=is%3Apr+author%3ARonid1+",
+  },
+  {
+    name: "Certification Portal",
+    tech: "React.JS | Django",
+    description:
+      "Implemented a complete Software Development Life Cycle including analysis, architecture, design and execution. Focusing on design patterns and object-oriented principals.",
+    link: "https://github.com/Ronid1/Certification_Portal",
+    demoGif: "Certs_Portal",
+    imageName: "Certs_Portal",
+  },
+  {
+    name: "Rubiks Cube",
+    tech: "JavaScript | Three.JS",
+    description:
+      "Rubiks cube simulator developed with 3D modeling and user interaction.",
+    link: "https://github.com/Ronid1/Rubiks_Cube",
+    demoGif: "Rubiks_cube",
+    imageName: "Rubiks_cube",
+  },
+  {
+    name: "Wordle",
+    tech: "React.JS | Next.JS",
+    description:
+      "Extended Wordle game with a flexible board size. Designed components using JSX and CSS and managed game logic with React Context. Built with a Next.JS server to fetch words and cross-check guesses.",
+    link: "https://github.com/Ronid1/Wordle",
+    demoGif: "wordle",
+    imageName: "Wordle",
+  },
+  {
+    name: "Maps backend",
+    tech: "Node.JS | Express.JS | MongoDB",
+    description:
+      "Full server-side application including managing the database in MongoDB and building a RESTful API with end-point routing, data validation, error handling and integration of an external API.",
+    link: "https://github.com/Ronid1/Maps_Backend",
+  },
+  {
+    name: "This Web Page",
+    tech: "React.JS | CSS",
+    description:
+      "Frontend focused project, built in react and designed using some Bootstrap and lots of CSS. Desktop and mobile friendly.",
+    link: "https://github.com/Ronid1/Ronid1.github.io",
+  },
 ];
